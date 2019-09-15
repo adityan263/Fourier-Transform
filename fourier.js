e7cf0666df950d5e4b475b346a9ef7ba1bcf6d2d
Aditya Neralkar <neralkarad15.it@coep.ac.in>
2019-09-15 19:49:17 +0530

    added fft wrapper function to do the preprocessing

--- fourier.js
+++ fourier.js
@@ -7,8 +7,8 @@ function dft(f) {
 		var x1, y1;
 		x1 = y1 = 0;
 		for (let k = 0; k < n; k++) {
-			x1 += f[k].x * cos(2*PI*k*x/n) + f[k].y * sin(2*PI*k*x/n);
-			y1 += f[k].x * -sin(2*PI*k*x/n) + f[k].y * cos(2*PI*k*x/n);
+			x1 += f[k].x * cos(2*PI*k*x/n) - f[k].y * sin(-2*PI*k*x/n);
+			y1 += f[k].x * sin(-2*PI*k*x/n) + f[k].y * cos(2*PI*k*x/n);
 		}
 		F[x] = {r:sqrt(x1*x1+y1*y1)/n, w:x, p:atan2(y1,x1)};
 	}
@@ -16,6 +16,22 @@ function dft(f) {
 }
 
 function fft(f) {
+	let n = f.length;
+	let x = pow(2,int(log(n)/log(2))+1) - n;
+	// padding
+	for (let i = 0; i < x; i++)
+		f.push(f[n-1]);
+	n += x;
+	F = actual_fft(f);
+	for (let i = 0; i < n; i++) {
+		let x1 = F[i].x;
+		let y1 = F[i].y;
+		F[i] = {r:sqrt(x1*x1+y1*y1)/n, w:i, p:atan2(y1,x1)};
+	}
+	return F;
+}
+
+function actual_fft(f) {
 	const n = f.length;
 	if (n == 1)
 		return f;
@@ -28,8 +44,8 @@ function fft(f) {
 		F.push({});
 		F.push({});
 	}
-	let Fodd = fft(odd);
-	let Feven = fft(even);
+	let Fodd = actual_fft(odd);
+	let Feven = actual_fft(even);
 	for (let i = 0; i < n/2; i++) {
 		let re = cos(2*-PI*i/n) * Fodd[i].x - sin(2*-PI*i/n) * Fodd[i].y;
 		let im = sin(2*-PI*i/n) * Fodd[i].x + cos(2*-PI*i/n) * Fodd[i].y;
--- sketch.js
+++ sketch.js
@@ -24,35 +24,11 @@ function mousePressed() {
 
 function mouseReleased() {
 	takingInput = time = 0;
-	let n = shape.length;
-	let x = pow(2,int(log(n)/log(2))+1) - n;
-	for (let i = 0; i < x; i++)
-		shape.push(shape[n-1]);
-	n = shape.length;
-	//circle1 = dft(shape);
-	circles = fft(shape);
-	for (let i = 0; i < n; i++) {
-		let x1 = circles[i].x;
-		let y1 = circles[i].y;
-		circles[i] = {r:sqrt(x1*x1+y1*y1)/n, w:i, p:atan2(y1,x1)};
-	}
+	circles = dft(shape);
+	n = circles.length;
 	circles.sort((a, b) => b.r - a.r);
-	//circle1.sort((a, b) => b.r - a.r);
 	skip = circles.length;
-	//console.log(circle1);
-	console.log(circles);
 }
-
-//function myInputEvent() {
-//	let s = int(abs(this.value()));
-//	if (s > shape.length)
-//		skip = shape.length;
-//	else if (s < 0)
-//		skip = 0;
-//	else
-//		skip = s;
-//	inp.value(skip);
-//}
 
 function drawCircles(circles) {
 	let px, py;
@@ -89,7 +65,7 @@ function draw() {
 		wave.push(p);
 	}
 	endShape();
-	if (wave.length == shape.length)
+	if (wave.length == circles.length)
 		wave = [];
-	time += 2*PI/shape.length;
+	time += 2*PI/circles.length;
 }
